d56b9e21b0be25449988518961be684ef18714c0
ming <ming222555@yahoo.com>
2025-07-27 09:53:01 +0800

    Html head add meta "viewport"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,15 +16,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Snacko snackkie" />
         <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Quiz on your knowledge of Malaysian snacks"
+        />
         <meta
           property="og:image"
           content="http://forfbshare.s3-website-ap-southeast-1.amazonaws.com/4_question-Three.jpg"
         />
         <meta property="og:url" content="https://limsimi2.vercel.app/" />
-        <title>Snacko snackkie</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body
         className="h-full"
