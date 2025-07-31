5df3d0ec5328c30f21b2e7f6f12f9b89408e2930
ming <ming222555@yahoo.com>
2025-07-31 20:51:24 +0800

    wip2

--- src/app/(quiz)/page.tsx
+++ src/app/(quiz)/page.tsx
@@ -2,31 +2,42 @@
 
 import { useState } from "react";
 
-import SectionCover from "../../ui/home/SectionCover";
+// import SectionCover from "../../ui/home/SectionCover";
 import SectionPrelude from "../../ui/home/SectionPrelude";
 import SectionQuestionSheet from "../../ui/home/SectionQuestionSheet";
 import BigButton from "../../ui/home/BigButton";
 
 export default function Home() {
-  const [isCover, setIsCover] = useState(true);
   const [isStartQuestion, setIsStartQuestion] = useState(false);
 
   return (
+    // <article
+    //   className={`text-center ${isCover ? "h-full flex flex-col" : "h-auto"}`}
+    //   style={{ outline: "11px solid green" }}
+    // >
+    //   {isCover ? (
+    //     <SectionCover>
+    //       <BigButton className="self-center" onClick={() => setIsCover(false)}>
+    //         Take the quiz
+    //       </BigButton>
+    //     </SectionCover>
+    //   ) : !isStartQuestion ? (
+    //     <SectionPrelude>
+    //       <BigButton onClick={() => setIsStartQuestion(true)}>
+    //         Prove it
+    //       </BigButton>
+    //     </SectionPrelude>
+    //   ) : (
+    //     <SectionQuestionSheet />
+    //   )}
+    // </article>
     <article
-      className={`text-center ${isCover ? "h-full flex flex-col" : "h-auto"}`}
+      className="text-center h-full"
       style={{ outline: "11px solid green" }}
     >
-      {isCover ? (
-        <SectionCover>
-          <BigButton className="self-center" onClick={() => setIsCover(false)}>
-            Take the quiz
-          </BigButton>
-        </SectionCover>
-      ) : !isStartQuestion ? (
+      {!isStartQuestion ? (
         <SectionPrelude>
-          <BigButton onClick={() => setIsStartQuestion(true)}>
-            Prove it
-          </BigButton>
+          <button onClick={() => setIsStartQuestion(true)}>Prove it</button>
         </SectionPrelude>
       ) : (
         <SectionQuestionSheet />
